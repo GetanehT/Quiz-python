da88f12ba773d009203199f666b4b00f0d568721
Getaneh Biratu <mrgetaneh@gmail.com>
2024-07-22 23:52:34 +0000

    adding free spaces for each of answers

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def run_quiz(questions):
 
         while not valid_input:  # Loop until valid input is received
             try:
-                answer = int(input("Enter the number of the correct answer:\n"))
+                answer = int(input("Enter the no of the correct answer:\n"))
 
                 if 1 <= answer <= len(question.options):
                     if question.options[answer - 1] == question.answer:
@@ -64,52 +64,52 @@ def run_quiz(questions):
 questions = [
     Question(
         "What is the chemical symbol for hydrogen?\n",
-        ["H", "He", "O", "N"],
+        ["H", "He", "O", "N""\n"],
         "H"
     ),
     Question(
         "What planet is known as the Earth's twin?\n",
-        ["Mars", "Venus", "Jupiter", "Saturn"],
+        ["Mars", "Venus", "Jupiter", "Saturn""\n"],
         "Venus"
     ),
     Question(
         "What is the hardest natural substance on Earth?\n",
-        ["Gold", "Iron", "Diamond", "Platinum"],
+        ["Gold", "Iron", "Diamond", "Platinum""\n"],
         "Diamond"
     ),
     Question(
         "Who developed the theory of general relativity?\n",
-        ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Nikola Tesla"],
+        ["Isaac Newto", "Albert Einstein", "Galilo Galili", "Nikol Tesla""\n"],
         "Albert Einstein"
     ),
     Question(
         "What is the largest organ in the human body?\n",
-        ["Heart", "Liver", "Skin", "Brain"],
+        ["Heart", "Liver", "Skin", "Brain""\n"],
         "Skin"
     ),
     Question(
         "Who was the first President of the United States?\n",
-        ["Jefferson", "Abraham Lincoln", "George Washington", "John Adams"],
+        ["Jefferson", "Abraham Lincol", "George Washington", "John Adams""\n"],
         "George Washington"
     ),
     Question(
         "In what year did the Titanic sink?\n",
-        ["1905", "1912", "1920", "1935"],
+        ["1905", "1912", "1920", "1935""\n"],
         "1912"
     ),
     Question(
         "Who was the British Prime Minister during World War II?\n",
-        ["Winston Churchill", "Chamberlain", "Attlee", "Harold Macmillan"],
+        ["Winston Churchill", "Chamberlain", "Attlee", "Harold Macmillan""\n"],
         "Winston Churchill"
     ),
     Question(
         "Which empire was ruled by Genghis Khan?\n",
-        ["Roman Empire", "Ottoman Empire", "Mongol Empire", "British Empire"],
+        ["Roman Empire", "Ottoman ", "Mongol Empire", "British Empire""\n"],
         "Mongol Empire"
     ),
     Question(
         "What was the main cause of the American Civil War?\n",
-        ["Taxation", "Slavery", "Territorial Expansion", "Trade Disputes"],
+        ["Taxation", "Slavery", "Territorial Expansion", "Trade Disputes""\n"],
         "Slavery"
     )
 ]
