632bb07b774dde456ed956ec3376f58b06000755
Getaneh Biratu <mrgetaneh@gmail.com>
2024-07-22 19:49:44 +0000

    Added enumeration and formatted printing of multiple-choice options in the run_quiz function

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def run_quiz(questions):
         which takes one parameter questions. 
         This parameter is expected to be a list of Question objects.
     """
-    score = 0
+    score = 0  
     incorrect_questions = []
 
 random.shuffle(questions)  # Shuffle the questions to randomize their order
