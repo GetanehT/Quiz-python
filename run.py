0587bdd91b745f5035b92a37e7104a2a4a5de1a9
Getaneh Biratu <mrgetaneh@gmail.com>
2024-07-22 16:24:38 +0000

    define the run function

--- run.py
+++ run.py
@@ -2,11 +2,20 @@ import random
 
 class Question: # Define the Question class
     def __init__(self, prompt, options, answer):
-        """
-        which takes one parameter questions. 
-        This parameter is expected to be a list of Question objects.
-        """
+       
         self.prompt = prompt
         self.options = options
         self.answer = answer
 
+# Define the run_quiz function
+def run_quiz(questions):
+    """
+        which takes one parameter questions. 
+        This parameter is expected to be a list of Question objects.
+    """
+    score = 0
+    incorrect_questions = []
+
+    random.shuffle(questions)  # Shuffle the questions to randomize their order
+
+    
